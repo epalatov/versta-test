5c3059136e69c22304a6c861f5b1406271222f9c
Gentim Dao <befrontender@gmail.com>
2017-04-05 17:08:43 +0700

    Adds the second section and footer

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,30 @@
 $(function() {
 
-	// Custom JS
 
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.offer__title', {
+  origin: 'bottom',
+  scale: 1,
+  distance: '100px',
+  duration: 1000,
+  easing: 'cubic-bezier(.31,.05,.18,.99)'
+}).reveal('.offer__subtitle', {
+  origin: 'bottom',
+  scale: 1,
+  delay: 300,
+  distance: '50px',
+  duration: 1000,
+  easing: 'cubic-bezier(.31,.05,.18,.99)'
+});
+
+sr.reveal('.for-whom__content', {
+  viewFactor: 1,
+  viewOffset: { top: 20 },
+  origin: 'bottom',
+  scale: 1,
+  distance: '80px',
+  duration: 1000,
+  easing: 'cubic-bezier(.31,.05,.18,.99)'
 });
